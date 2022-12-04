5a1c37a3ce027f6107afa408c7c50a048a89ade1
Jesper Turesson <Jturesson90@gmail.com>
2022-12-04 07:36:18 +0100

    Day 4

--- Assets/AdventOfCode/Runtime/Days/Day04.cs
+++ Assets/AdventOfCode/Runtime/Days/Day04.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using JTuresson.AdventOfCode;
 
 namespace AdventOfCode.Days
 {
@@ -8,12 +7,52 @@ namespace AdventOfCode.Days
     {
         public string PuzzleA(string input)
         {
-            return input;
+            var pairs = ParseInput.ParseAsMultiArray(input.Trim(), '\n', ',');
+            var count = Count(pairs, Contains);
+            return count.ToString();
         }
 
         public string PuzzleB(string input)
         {
-            return input;
+            var pairs = ParseInput.ParseAsMultiArray(input.Trim(), '\n', ',');
+            var count = Count(pairs, Overlaps);
+            return count.ToString();
+        }
+
+        private static bool Contains(int x1, int x2, int y1, int y2) => x1 >= y1 && x2 <= y2;
+        private static bool Overlaps(int x1, int x2, int y1, int y2) => x1 >= y1 && x1 <= y2 || x2 >= y1 && x2 <= y2;
+
+        private int Count(string[][] pairs, Func<int, int, int, int, bool> callback)
+        {
+            var count = 0;
+            foreach (var pair in pairs)
+            {
+                if (pair.Length != 2)
+                {
+                    throw new ArgumentException("There is not exactly 2 in the pair");
+                }
+
+                var left = pair[0].Split('-');
+                var right = pair[1].Split('-');
+                var (x1, x2) = (int.Parse(left[0]), int.Parse(left[1]));
+                var (y1, y2) = (int.Parse(right[0]), int.Parse(right[1]));
+                if (callback(x1, x2, y1, y2) && callback(y1, y2, x1, x2))
+                {
+                    count++;
+                }
+                else
+                    if (callback(x1, x2, y1, y2))
+                    {
+                        count++;
+                    }
+                    else
+                        if (callback(y1, y2, x1, x2))
+                        {
+                            count++;
+                        }
+            }
+
+            return count;
         }
     }
 }
